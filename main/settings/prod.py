e7114425eb9aa2fc73b49beb29d3858020ba8cad
gointer <gointer@ukr.net>
2016-02-24 11:17:06 +0300

    Heroku changing

--- main/settings/prod.py
+++ main/settings/prod.py
@@ -2,4 +2,7 @@ from .common import *
 
 DEBUG = False
 
-INSTALLED_APPS += ("gunicorn", )
+INSTALLED_APPS += ("gunicorn", "dj_database_url",)
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
